93046673cb0809ac7ed9413ab35c81ec833ab34c
callmecavs <callmecavs@gmail.com>
2017-03-24 14:39:00 -0400

    swap webpack for rollup, preserving module name

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+
+const info = require('./package.json')
+
+const config = {
+  entry: 'src/tweezer.js',
+  plugins: [
+    resolve(),
+    babel({
+      babelrc: false,
+      presets: [
+        [
+          'env', {
+            modules: false,
+            targets: {
+              browsers: ['last 2 versions']
+            }
+          }
+        ]
+      ]
+    })
+  ],
+  targets: [
+    {
+      dest: info.main,
+      format: 'umd',
+      moduleName: 'Tweezer'
+    }, {
+      dest: info.module,
+      format: 'es'
+    }
+  ]
+}
+
+export default config
